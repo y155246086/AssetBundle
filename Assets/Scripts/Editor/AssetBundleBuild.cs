d02990fccccaffcb58849d82f578a57bf09acdc0
fredyang <155246086@qq.com>
2017-11-15 17:34:50 +0800

    test

---
+++ Assets/Scripts/Editor/AssetBundleBuild.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundleBuild
+{
+
+    public static string platform = string.Empty;
+    static List<string> paths = new List<string>();
+    static List<string> files = new List<string>();
+    static List<AssetBundleBuild> maps = new List<AssetBundleBuild>();
+    public static string sourcePath = Application.dataPath + "/Resources";
+    private static Dictionary<string, int> map;
+    const string AssetBundlesOutputPath = "Assets/StreamingAssets";
+
+    [MenuItem("Tools/AssetBundle/Build")]
+    public static void BuildAssetBundle()
+    {
+        Caching.CleanCache();
+        ClearAssetBundlesName();
+
+        Pack(sourcePath);
+
+        string outputPath = Path.Combine(AssetBundlesOutputPath, Platform.GetPlatformFolder(EditorUserBuildSettings.activeBuildTarget));
+        if (!Directory.Exists(Application.dataPath + "/../Assetbundle"))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/../Assetbundle");
+        }
+        Debug.Log(outputPath);
+        //根据BuildSetting里面所激活的平台进行打包
+        // BuildPipeline.BuildAssetBundles(outputPath, 0, EditorUserBuildSettings.activeBuildTarget);
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/../Assetbundle", BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android);
+        BuildFileIndex();
+        AssetDatabase.Refresh();
+        Debug.Log("打包完成");
+
+    }
+
+    /// <summary>
+    /// 清除之前设置过的AssetBundleName，避免产生不必要的资源也打包
+    /// 之前说过，只要设置了AssetBundleName的，都会进行打包，不论在什么目录下
+    /// </summary>
+    static void ClearAssetBundlesName()
+    {
+        int length = AssetDatabase.GetAllAssetBundleNames().Length;
+        Debug.Log(length);
+        string[] oldAssetBundleNames = new string[length];
+        for (int i = 0; i < length; i++)
+        {
+            oldAssetBundleNames[i] = AssetDatabase.GetAllAssetBundleNames()[i];
+        }
+
+        for (int j = 0; j < oldAssetBundleNames.Length; j++)
+        {
+            AssetDatabase.RemoveAssetBundleName(oldAssetBundleNames[j], true);
+        }
+        length = AssetDatabase.GetAllAssetBundleNames().Length;
+        Debug.Log(length);
+    }
+
+    static void Pack(string source)
+    {
+        DirectoryInfo folder = new DirectoryInfo(source);
+        FileSystemInfo[] files = folder.GetFileSystemInfos();
+        int length = files.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (files[i] is DirectoryInfo)
+            {
+                Pack(files[i].FullName);
+            }
+            else
+            {
+                if (!files[i].Name.EndsWith(".meta"))
+                {
+                    file(files[i].FullName);
+                }
+            }
+        }
+    }
+
+    static void file(string source)
+    {
+        string _source = Replace(source);
+        string _assetPath = "Assets" + _source.Substring(Application.dataPath.Length);
+        string _assetPath2 = _source.Substring(Application.dataPath.Length + 1);
+        //Debug.Log (_assetPath);
+
+        //在代码中给资源设置AssetBundleName
+        AssetImporter assetImporter = AssetImporter.GetAtPath(_assetPath);
+        string assetName = _assetPath2.Substring(_assetPath2.IndexOf("/") + 1);
+        assetName = assetName.Replace(Path.GetExtension(assetName), ".unity3d");
+        //Debug.Log (assetName);
+        assetImporter.assetBundleName = assetName;
+    }
+
+    static string Replace(string s)
+    {
+        return s.Replace("\\", "/");
+    }
+
+    static void BuildFileIndex()
+    {
+        string resPath = AppDataPath + "/StreamingAssets/";
+        resPath = Application.dataPath + "/../Assetbundle";
+        ///----------------------创建文件列表-----------------------
+        string newFilePath = resPath + "/files.txt";
+        if (File.Exists(newFilePath)) File.Delete(newFilePath);
+
+        paths.Clear(); files.Clear();
+        Recursive(resPath);
+
+        FileStream fs = new FileStream(newFilePath, FileMode.CreateNew);
+        StreamWriter sw = new StreamWriter(fs);
+        for (int i = 0; i < files.Count; i++)
+        {
+            string file = files[i];
+            string ext = Path.GetExtension(file);
+            if (file.EndsWith(".meta") || file.Contains(".DS_Store")) continue;
+
+            string md5 = md5file(file);
+            string value = file.Replace(resPath, string.Empty);
+            FileInfo info = new FileInfo(file);
+            sw.WriteLine(value + "|" + md5 + "|" + info.Length);
+        }
+        sw.Close(); fs.Close();
+    }
+    /// <summary>
+    /// 数据目录
+    /// </summary>
+    static string AppDataPath
+    {
+        get { return Application.dataPath.ToLower(); }
+    }
+
+    /// <summary>
+    /// 遍历目录及其子目录
+    /// </summary>
+    static void Recursive(string path)
+    {
+        string[] names = Directory.GetFiles(path);
+        string[] dirs = Directory.GetDirectories(path);
+        foreach (string filename in names)
+        {
+            string ext = Path.GetExtension(filename);
+            if (ext.Equals(".meta")) continue;
+            files.Add(filename.Replace('\\', '/'));
+        }
+        foreach (string dir in dirs)
+        {
+            paths.Add(dir.Replace('\\', '/'));
+            Recursive(dir);
+        }
+    }
+    /// <summary>
+    /// 计算字符串的MD5值
+    /// </summary>
+    public static string md5(string source)
+    {
+        MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+        byte[] data = System.Text.Encoding.UTF8.GetBytes(source);
+        byte[] md5Data = md5.ComputeHash(data, 0, data.Length);
+        md5.Clear();
+
+        string destString = "";
+        for (int i = 0; i < md5Data.Length; i++)
+        {
+            destString += System.Convert.ToString(md5Data[i], 16).PadLeft(2, '0');
+        }
+        destString = destString.PadLeft(32, '0');
+        return destString;
+    }
+
+    /// <summary>
+    /// 计算文件的MD5值
+    /// </summary>
+    public static string md5file(string file)
+    {
+        try
+        {
+            FileStream fs = new FileStream(file, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(fs);
+            fs.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("md5file() fail, error:" + ex.Message);
+        }
+    }
+}
+public class Platform
+{
+    public static string GetPlatformFolder(BuildTarget target)
+    {
+        switch (target)
+        {
+            case BuildTarget.Android:
+                return "Android";
+            case BuildTarget.iOS:
+                return "IOS";
+            case BuildTarget.WebPlayer:
+                return "WebPlayer";
+            case BuildTarget.StandaloneWindows:
+            case BuildTarget.StandaloneWindows64:
+                return "Windows";
+            case BuildTarget.StandaloneOSXIntel:
+            case BuildTarget.StandaloneOSXIntel64:
+            case BuildTarget.StandaloneOSXUniversal:
+                return "OSX";
+            default:
+                return null;
+        }
+    }
+}
